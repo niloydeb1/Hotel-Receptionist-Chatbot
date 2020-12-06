1bd5b68bfcbfb642d619125196fc146123cb0aa7
niloydeb1 <34337108+niloydeb1@users.noreply.github.com>
2020-12-06 22:43:47 +0600

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,58 @@
+import random
+import json
+
+import torch
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "ReceptionBot"
+print("Welcome to Grand Bangladesh Hotel. I am your receptionist bot. Let's chat! (type 'quit' to exit)")
+flag = 0
+while True:
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        flag = 0
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand...")
+        flag = flag + 1
+        if flag == 3:
+            print("Please try to Contact us:\n Tel: +8802985910454\n Email: info@grandbangladeshhotel.com")
+            flag = 0
